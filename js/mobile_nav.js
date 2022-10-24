ce7e20a8422bfeff97a9bc28002fac82396d1b58
Blake Weissman <contact@blakeweissman.com>
2022-10-23 20:54:59 -0400

    Re-formatted all script files

--- js/mobile_nav.js
+++ js/mobile_nav.js
@@ -1,44 +1,44 @@
-$( document ).ready(function() {
-    if ($(window).width() >= 1086){
-        $( "#menu-icon" ).hide();
-        $( "#menu-icon-close" ).hide();
-        $( "ul" ).show();
-    }  
-    if ($(window).width() <= 1086){	
-        $( "#menu-icon" ).show();
-        $( "ul" ).hide();
-        $( "#menu-icon-close" ).hide();
-        $( "#menu-icon" ).click(function() {
-            $( "ul" ).show();
-            $( "#menu-icon" ).hide();
-            $( "#menu-icon-close" ).show();
-        });
-        $('#menu-icon-close').click(function() {
-        $( "ul" ).hide();
-        $( "#menu-icon" ).show();
-        $( "#menu-icon-close" ).hide();
-        });
-    }
-    $(window).resize(function(){
-        if ($(window).width() <= 1086){	
-            $( "#menu-icon" ).show();
-            $( "ul" ).hide();
-            $( "#menu-icon-close" ).hide();
-            $( "#menu-icon" ).click(function() {
-            $( "ul" ).show();
-            $( "#menu-icon" ).hide();
-            $( "#menu-icon-close" ).show();
-            });
-            $('#menu-icon-close').click(function() {
-            $( "ul" ).hide();
-            $( "#menu-icon" ).show();
-            $( "#menu-icon-close" ).hide();
-            });
-        }	
-        if ($(window).width() >= 1086){	
-            $( "#menu-icon" ).hide();
-            $( "#menu-icon-close" ).hide();
-            $( "ul" ).show();
-        }  
-    });
+$(document).ready(function () {
+	if ($(window).width() >= 1086) {
+		$("#menu-icon").hide();
+		$("#menu-icon-close").hide();
+		$("ul").show();
+	}
+	if ($(window).width() <= 1086) {
+		$("#menu-icon").show();
+		$("ul").hide();
+		$("#menu-icon-close").hide();
+		$("#menu-icon").click(function () {
+			$("ul").show();
+			$("#menu-icon").hide();
+			$("#menu-icon-close").show();
+		});
+		$("#menu-icon-close").click(function () {
+			$("ul").hide();
+			$("#menu-icon").show();
+			$("#menu-icon-close").hide();
+		});
+	}
+	$(window).resize(function () {
+		if ($(window).width() <= 1086) {
+			$("#menu-icon").show();
+			$("ul").hide();
+			$("#menu-icon-close").hide();
+			$("#menu-icon").click(function () {
+				$("ul").show();
+				$("#menu-icon").hide();
+				$("#menu-icon-close").show();
+			});
+			$("#menu-icon-close").click(function () {
+				$("ul").hide();
+				$("#menu-icon").show();
+				$("#menu-icon-close").hide();
+			});
+		}
+		if ($(window).width() >= 1086) {
+			$("#menu-icon").hide();
+			$("#menu-icon-close").hide();
+			$("ul").show();
+		}
+	});
 });
